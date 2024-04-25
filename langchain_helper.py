f639034ddf13a90a80ee1f2baad883f40d3e4233
aagneye-syam <saagneye2003@gmail.com>
2024-04-25 22:42:32 +0530

    FAISS imported and used

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,17 +1,17 @@
 from langchain.llms import GooglePalm
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-llm = GooglePalm(google_api_key=os.environ["API_KEY"], temperature= 0.1)
+llm = GooglePalm(google_api_key=os.environ["API_KEY"], temperature=0.1)
 
-loader = CSVLoader(file_path='Q&A.csv',source_column='prompt')
+loader = CSVLoader(file_path='Q&A.csv', source_column='prompt')
 data = loader.load()
 
-embeddings = HuggingFaceInstructEmbeddings()
+instructor_embeddings = HuggingFaceInstructEmbeddings()
 
-
-
+vectordb = FAISS.from_documents(documents=docs, embedding=instructor_embeddings)
