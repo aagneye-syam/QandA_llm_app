a069e68779d31b6c17ea69a96c647ec574e2800a
aagneye-syam <saagneye2003@gmail.com>
2024-04-25 15:18:02 +0530

    Google API integrated

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,4 +1,12 @@
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+llm = GooglePalm(google_api_key=os.environ["API_KEY"], temperature= 0.1)
+
+result = llm(input('Type here '))
+
+print(result)
