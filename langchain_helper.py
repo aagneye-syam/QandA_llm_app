8d01c9957d1ed8ae56ce6a240b544849b1b1fc3b
aagneye-syam <saagneye2003@gmail.com>
2024-04-26 11:53:40 +0530

    prompt and chain creation done in a function

--- langchain_helper.py
+++ langchain_helper.py
@@ -17,17 +17,25 @@ instructor_embeddings = HuggingFaceInstructEmbeddings(
 
 vectordb_file_path = "faiss_index"
 
+
 def create_vector_db():
-    loader = CSVLoader(file_path='Q&A.csv', source_column='prompt')
-    docs = loader.load()
-    vectordb = FAISS.from_documents(documents=docs, embedding=instructor_embeddings)
+
+    loader = CSVLoader(file_path='codebasics_faqs.csv', source_column="prompt")
+    data = loader.load()
+
+    vectordb = FAISS.from_documents(documents=data,
+                                    embedding=instructor_embeddings)
+
     vectordb.save_local(vectordb_file_path)
 
 
-retriever = vectordb.as_retriever
-rdoc = retriever.get_relevant_documents("for how long is this program")
+def get_qa_chain():
 
-prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
     If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
 
@@ -35,17 +43,19 @@ prompt_template = """Given the following context and a question, generate an ans
 
     QUESTION: {question}"""
 
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
 
-RetrievalQA(llm=llm, chain_type="stuff",
-            retriever=retriever,
-            input_key="query",
-            return_source_documents=True,
-            chain_type_kwargs={"prompt": PROMPT})
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     create_vector_db()
-    
