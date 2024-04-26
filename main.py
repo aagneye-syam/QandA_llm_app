a3c2df2fc1ded16398a919023fe14cdf4ab5c295
aagneye-syam <saagneye2003@gmail.com>
2024-04-26 12:12:00 +0530

    langchain_helper imported and connected to text field

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
-
+from langchain_helper import create_vector_db, get_qa_chain
 
 st.title("MuLearn Q&A?")
 
-btn = st.button("Create Knowledgebase")
-if btn:
-        pass
 
 question = st.text_input("Question : ")
 
 if question:
-        pass
+        chain = get_qa_chain()
+        response = chain(question)
+
+        st.header("Answer: ")
+        st.write(response["result"])
