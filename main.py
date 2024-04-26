214080ef2f4e5865d783d4d929efdfaf111baac0
aagneye-syam <saagneye2003@gmail.com>
2024-04-26 12:00:43 +0530

    input field and button created

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import streamlit as st
 
 st.title("MuLearn Q&A?")
 
+btn = st.button("Create Knowledgebase")
+if btn:
+        pass
 
+question = st.text_input("Question : ")
+
+if question:
+        pass
