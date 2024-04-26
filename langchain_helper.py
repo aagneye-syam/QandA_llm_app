16c8ab22b9ecb6f2f01cffdfe127c1a68f396278
aagneye-syam <saagneye2003@gmail.com>
2024-04-26 11:35:37 +0530

    PromptTemplate setup

--- langchain_helper.py
+++ langchain_helper.py
@@ -3,6 +3,7 @@ from langchain.llms import GooglePalm
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -22,8 +23,22 @@ vectordb = FAISS.from_documents(
 retriever = vectordb.as_retriever
 rdoc = retriever.get_relevant_documents("for how long is this program")
 
+prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
 RetrievalQA(llm=llm, chain_type="stuff",
             retriever=retriever,
             input_key="query",
             return_source_documents=True,
             chain_type_kwargs={"prompt": PROMPT})
+
+chain("")
