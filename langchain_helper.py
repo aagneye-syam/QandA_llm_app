e403ad18193e1ebc57ebcb9ab0682b0c007eae32
aagneye-syam <saagneye2003@gmail.com>
2024-04-26 11:29:07 +0530

    prompting to llm

--- langchain_helper.py
+++ langchain_helper.py
@@ -2,6 +2,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import GooglePalm
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 import os
 
@@ -12,9 +13,17 @@ llm = GooglePalm(google_api_key=os.environ["API_KEY"], temperature=0.1)
 loader = CSVLoader(file_path='Q&A.csv', source_column='prompt')
 data = loader.load()
 
-instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+instructor_embeddings = HuggingFaceInstructEmbeddings(
+    model_name="hkunlp/instructor-large")
 
-vectordb = FAISS.from_documents(documents=docs, embedding=instructor_embeddings)
+vectordb = FAISS.from_documents(
+    documents=docs, embedding=instructor_embeddings)
 
 retriever = vectordb.as_retriever
 rdoc = retriever.get_relevant_documents("for how long is this program")
+
+RetrievalQA(llm=llm, chain_type="stuff",
+            retriever=retriever,
+            input_key="query",
+            return_source_documents=True,
+            chain_type_kwargs={"prompt": PROMPT})
